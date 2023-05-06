7bde831f115636df22db71626585b37d7b756d84
marianagrana2 <mariana.grana2@gmail.com>
2023-05-06 17:25:28 -0600

    feat:Ejercicio2ES6conWhile

--- es6tarea/es6.js
+++ es6tarea/es6.js
@@ -11,19 +11,31 @@ const app = student.app;
 console.log(id , name , app);*/
 
 //Con Destructuring
-console.log("Con Destructuring.")
+/*console.log("Con Destructuring.")
 const {id, name , app} = student; 
-console.log(id,name, app);
+console.log(id,name, app);*/
 
 
-/*2. Del siguiente arreglo obtener todos los estudiantes con calificacion mayor a 8 y que estan activos en devf.
-Hacerlo con un ciclo while, con un ciclo for y con ES6 (filter, forEach, map).
+//2. Del siguiente arreglo obtener todos los estudiantes con calificacion mayor a 8 y que estan activos en devf.
+//Hacerlo con un ciclo while, con un ciclo for y con ES6 (filter, forEach, map).
 
-const studenrs = [
+const students = [
     { id: 1, name: 'elsa', app: 'sayjayin', grade: 10, isActive: true  },
     { id: 2, name: 'elsa', app: 'zapatito', grade: 9, isActive: false  },
     { id: 3, name: 'elsa', app: 'zapote', grade: 7, isActive: true },
     { id: 4, name: 'elsa', app: 'zapote', grade: 8, isActive: false },
     { id: 5, name: 'elsa', app: 'zapote', grade: 6, isActive: true },
     { id: 6, name: 'elsa', app: 'zapote', grade: 9, isActive: false },
-] */
+] 
+//Ciclo While
+let i= 0;
+const studentsActivesAndGoodGrades = []; //Se inicia con un arreglo vacio.
+
+while (i < students.length){
+    if(students[i].grade > 8 && students[i].isActive){
+        studentsActivesAndGoodGrades.push(students[i]);   //Agrega la nueva información al arreglo.
+    }
+    i++; // Agregarse a fuera para evitar bucle infinito
+}
+console.log(studentsActivesAndGoodGrades);
+
