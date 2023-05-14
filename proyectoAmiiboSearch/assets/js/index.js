29a74028e4ab4a9d20b73aca45325e81fbbe942a
marianagrana2 <mariana.grana2@gmail.com>
2023-05-14 14:38:30 -0600

    Docs: Creación Archivos, Base html y const en JS

---
+++ proyectoAmiiboSearch/assets/js/index.js
@@ -0,0 +1,7 @@
+//Elementos html 
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#input');
+const searchButton = document.querySelector('#searchButton');
+const results = document.querySelector('#results');
+const amiiboInfo = document.querySelector('#amiiboContainer');
+
